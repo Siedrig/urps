77d62c617ad7697e03de2dbfbc51b35111934da5
Siedrig <112951304+Siedrig@users.noreply.github.com>
2024-10-07 02:02:13 -0300

    consertei minha nav

--- header.js
+++ header.js
@@ -1,13 +1,20 @@
-let appHeader = `
+let appHeader =
+`
 
-    <nav id="navegacao">
-        <ul>
-            <li><a href="index.html">Página Inicial</a></li>
-            <li><a href="entradaform.html"> Investigadores de Campo</a></li>
-            <li><a href="manifestacoes.html">Manifestações conhecidas</a></li>
-            <li><a href="progresso.html">Acompanhe nossa missão</a></li>
-        </ul>     
-    </nav>
- 
+   <nav id="navegacao">
+            <ul>
+                <li><a href="index.html">Página Inicial</a></li>
+                <li><a href="entradaform.html">Investigadores de Campo</a></li>
+                <li><a href="manifestacoes.html">Manifestações conhecidas</a></li>
+                <li><a href="progresso.html">Acompanhe nossa missão</a></li>
+            </ul>     
+        </nav>
 `;
 document.getElementById("app-header").innerHTML = appHeader;
+
+[].forEach.call(document.querySelectorAll('a'), function(elem) {
+    if (elem.pathname === window.location.pathname)
+      elem.classList.add('active')
+    else
+      elem.classList.remove('active')
+  })
