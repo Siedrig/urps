ac09f44b79301a483d62776283bf78d24d743a1b
Siedrig <112951304+Siedrig@users.noreply.github.com>
2024-10-07 09:28:51 -0300

    consertei minha nav v2

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,15 @@
+let appHeader =
+`
+
+   <nav id="navegacao">
+            <ul>
+                <li><a href="index.html">Página Inicial</a></li>
+                <li><a href="entradaform.html">Investigadores de Campo</a></li>
+                <li><a href="manifestacoes.html">Manifestações conhecidas</a></li>
+                <li><a href="progresso.html">Acompanhe nossa missão</a></li>
+            </ul>     
+        </nav>
+`;
 document.getElementById("app-header").innerHTML = appHeader;
 
 [].forEach.call(document.querySelectorAll('a'), function(elem) {
